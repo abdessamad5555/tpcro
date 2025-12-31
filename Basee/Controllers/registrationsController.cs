b9c69977d0c6fd53b2852b16137838400ad13033
abdessamad5555 <abdessamadelouali91@gmail.com>
2025-12-31 12:01:40 +0100

    Initial commit - Basee projectt

--- Basee/Controllers/registrationsController.cs
+++ Basee/Controllers/registrationsController.cs
@@ -1,157 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Basee.Data;
+using Basee.Mappers;
+using Basee.ViewModel;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Basee.Data;
-using Basee.Models;
 
-namespace Basee.Controllers
+public class RegistrationsController : Controller
 {
-    public class registrationsController : Controller
+    private readonly BaseeContext _context;
+
+    public RegistrationsController(BaseeContext context)
     {
-        private readonly BaseeContext _context;
+        _context = context;
+    }
 
-        public registrationsController(BaseeContext context)
+    // GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(registrationVM vm)
+    {
+        if (vm.password != vm.confpass)
         {
-            _context = context;
+            ModelState.AddModelError("confpass", "Les mots de passe ne correspondent pas");
+            return View(vm);
         }
 
-        // GET: registrations
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.registration.ToListAsync());
-        }
+        if (!ModelState.IsValid)
+            return View(vm);
 
-        // GET: registrations/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        var entity = RegistrationMapper.ToEntity(vm);
+        _context.registration.Add(entity);
+        await _context.SaveChangesAsync();
 
-            var registration = await _context.registration
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (registration == null)
-            {
-                return NotFound();
-            }
-
-            return View(registration);
-        }
-
-        // GET: registrations/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: registrations/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,login,password,confpass,nom,prenom,tentetive,bloque")] registration registration)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(registration);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(registration);
-        }
-
-        // GET: registrations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var registration = await _context.registration.FindAsync(id);
-            if (registration == null)
-            {
-                return NotFound();
-            }
-            return View(registration);
-        }
-
-        // POST: registrations/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,login,password,confpass,nom,prenom,tentetive,bloque")] registration registration)
-        {
-            if (id != registration.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(registration);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!registrationExists(registration.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(registration);
-        }
-
-        // GET: registrations/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var registration = await _context.registration
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (registration == null)
-            {
-                return NotFound();
-            }
-
-            return View(registration);
-        }
-
-        // POST: registrations/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var registration = await _context.registration.FindAsync(id);
-            if (registration != null)
-            {
-                _context.registration.Remove(registration);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool registrationExists(int id)
-        {
-            return _context.registration.Any(e => e.Id == id);
-        }
+        return RedirectToAction("Index");
     }
 }
---
+++ Basee/Mappers/RegistrationMapper.cs
@@ -0,0 +1,43 @@
+﻿using Basee.Models;
+using Basee.ViewModel;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Basee.Mappers
+{
+    public static class RegistrationMapper
+    {
+        // ViewModel → Entity
+        public static registration ToEntity(registrationVM vm)
+        {
+            return new registration
+            {
+                login = vm.login,
+                password = HashPassword(vm.password),
+                nom = vm.nom,
+                prenom = vm.prenom,
+                tentetive = 0,
+                bloque = false
+            };
+        }
+
+        // Entity → ViewModel (optionnel)
+        public static registrationVM ToViewModel(registration entity)
+        {
+            return new registrationVM
+            {
+                login = entity.login,
+                nom = entity.nom,
+                prenom = entity.prenom
+            };
+        }
+
+        // Hash mot de passe (simple mais propre)
+        private static string HashPassword(string password)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
